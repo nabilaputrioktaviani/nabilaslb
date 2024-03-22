192af78c5a7f0c77231d63cb47a70d6c039b8dea
nabilaputrioktaviani <164316523+nabilaputrioktaviani@users.noreply.github.com>
2024-03-22 09:52:54 +0700

    Add files via upload
    Sekolah Slb

---
+++ page/paging.php
@@ -0,0 +1,18 @@
+<?php
+    $page=(isset($_GET['page']))?$_GET['page']:"main";
+    switch ($page){
+        case 'menu':include "page/home.html";break;
+        case 'sejarah':include "page/sejarah.html";break;
+        case 'visi':include "page/visimisi.html";break;
+        case 'tujuan':include "page/tujuan.html";break;
+        case 'about':include "page/pembuatan.html";break;
+        case 'contact':include "page/contact.html";break;
+        case 'gallery':include "page/kegiatan.html";break;
+        case 'datapribadi':include "page/datapribadi.html";break;
+        case 'ruangkelas':include "page/ruangkelas";break;
+        
+        case 'main':
+        default:include"page/home.html";
+
+    }
+?>
